18e3ca2beba5e13bb44511623c543b1c4e72ed1a
erikqb3 <erikqb3@gmail.com>
2022-01-06 17:21:32 -0700

    Week1 stuff

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function prepStuff() {
+  let links_array = [
+    {label: "Week1 notes",
+    url: "Notes/Week1/story_editor.html"
+    },
+    {label: "TestRun",
+  url:"https://myheroacademia.fandom.com/wiki/List_of_Characters"}
+  ];
+  return links_array;
+}
+
+function addLinks() {
+  let links_array = prepStuff();
+  let linksList_element = document.getElementById("linksList");
+  console.log(linksList_element)
+  for (i=0; i<links_array.length; i++) {
+    console.log(links_array[i]);
+    listItem_element = document.createElement("li");
+    listItem_link = document.createElement("a");
+    linksList_element.appendChild(listItem_element);
+    listItem_element.appendChild(listItem_link);
+    listItem_link.innerHTML = links_array[i].label;
+    listItem_link.href =links_array[i].url;
+
+
+
+  }
+}
+
+function main() {
+  addLinks();
+}
+
+main();
